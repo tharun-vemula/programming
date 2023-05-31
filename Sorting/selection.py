c53c5853c3d843e613c241ee939a73e1aa1d4b92
Tharun Vemula <tharunvind@gmail.com>
2023-05-31 12:33:34 +0530

    Selection sort

---
+++ Sorting/selection.py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+
+arr = [4, 3, 1, 7, 54]
+selection_sort(arr)
+print(arr)
