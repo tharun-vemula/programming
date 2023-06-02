f16e4ed5955640bc4931a43abc3527b3bc5a635e
Tharun Vemula <tharunvind@gmail.com>
2023-06-02 20:45:26 +0530

    Add Edge class

--- Graph/graph.py
+++ Graph/graph.py
@@ -12,4 +12,33 @@ class Vertex:
         return self._element
 
     def __hash__(self):
+        """will allow vertex to be used a kep in a map"""
         return hash(id(self))
+
+
+class Edge:
+    """Lightweight structure for a graph"""
+
+    __slots__ = "_origin", "_destination", "_element"
+
+    def __init__(self, origin, destination, element):
+        """Do not call constructor directly. Use Graph.insert_edge(u,v,x)"""
+        self._element = element
+        self._origin = origin
+        self._destination = destination
+
+    def endpoints(self):
+        """Return (u,v) tuple for vertices u and v"""
+        return (self._origin, self._destination)
+
+    def opposite(self, v):
+        """Return vertex that is opposite to v"""
+        return self._origin if v is self._destination else self._destination
+
+    def element(self):
+        """Return element associated with this edge"""
+        return self._element
+
+    def __hash__(self):
+        """will allow vertex to be used a kep in a map"""
+        return hash((self._origin, self._destination))
