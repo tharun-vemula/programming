5dba85b22148434667407692087dc2edc276a74f
Tharun Vemula <tharunvind@gmail.com>
2023-06-03 12:35:57 +0530

    BFS

--- Graph/traversals.py
+++ Graph/traversals.py
@@ -7,3 +7,17 @@ def DFS(g: Graph, u: Vertex, discovered):
         if v not in discovered:
             discovered[v] = e
             DFS(g, v, discovered)
+
+
+def BFS(g: Graph, s: Vertex, discovered):
+    level = [s]
+
+    while len(level) > 0:
+        next_level = []
+        for u in level:
+            for e in g.incident_edges(s):
+                v = e.opposite(u)
+                if v not in discovered:
+                    discovered[v] = e
+                    next_level.append(v)
+        level = next_level
